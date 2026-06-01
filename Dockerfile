fe93670c306c78218bcef95e12fd6852cbe17194
spi-u <m2111815@edu.misis.ru>
2026-06-01 16:16:46 +0300

    chore: use docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:24.15.0-slim AS builder
+
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci
+COPY . .
+RUN npm run build
+
+FROM node:24.15.0-slim AS runner
+WORKDIR /app
+
+ENV NODE_ENV=production
+COPY package*.json ./
+
+RUN npm ci --omit=dev
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 3000
+CMD ["node", "dist/main"]
--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,12 +1,15 @@
 import { config } from 'dotenv';
+import { join } from 'path';
 import { DataSource } from 'typeorm';
 
 config();
 
+const ext = __filename.endsWith('.ts') ? 'ts' : 'js';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DB_URL ?? 'postgres://todo:todo@localhost:5432/todo',
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: [join(__dirname, '..', `**/*.entity.${ext}`)],
+  migrations: [join(__dirname, '..', `database/migrations/*.${ext}`)],
   synchronize: false,
 });
